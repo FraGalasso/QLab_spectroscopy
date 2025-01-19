cfba02c75ff1a3dd83db8db5380b6d3b07c7db25
Francesco Galasso <francesco.galasso7@studio.unibo.it>
2025-01-19 16:13:18 +0100

    add modld data

--- data_prep_lecroy.py
+++ data_prep_lecroy.py
@@ -6,19 +6,27 @@ import functions as fn
 
 '''This code reads the csv files, crops them to a window where the piezo voltage is just increasing,
 removes NaNs and fits the piezo data linearly. It then saves a csv file with  4 columns: timestamps,
-volt_laser, volt_piezo, piezo_fitted.'''
+transmission, volt_piezo, piezo_fitted.'''
+
+#####################################################################################################
+# section with file specific values
 
 # Define the folder and file paths
-folder_name = 'data10'
-title = 'no_absorption00000'
+folder_name = 'data_temp'
+title = 'mod_LD00001'
 df = pd.DataFrame()
 # Import single channel files
-channel_files = [f'{folder_name}/C{i}{title}.csv' for i in range(1, 4)]
+channel_files = [f'{folder_name}/raw/C{i}{title}.csv' for i in range(1, 4)]
 # Define channel names
 column_titles = ['volt_laser', 'volt_piezo', 'volt_ld']
 os.makedirs(f"{folder_name}/clean_data", exist_ok=True)
 os.makedirs(f"{folder_name}/figures/simple_plots_time", exist_ok=True)
-print(title)
+# Boolean to activate extra cropping, if necessary:
+cropping = False
+# You have to set the mask variable to what you need in the code
+# If you are using more than 3 channels you have to modify the rest of the code
+
+#####################################################################################################
 
 # Loop through each file in the file_paths
 for i, channel in enumerate(channel_files):
@@ -37,6 +45,7 @@ for i, channel in enumerate(channel_files):
     # Write channel reading to dataframe
     df[column_titles[i]] = data.iloc[:, 1]
 
+
 df = df.dropna()
 
 # Producing single numpy arrays for manipulation with functions
@@ -46,17 +55,16 @@ volt_laser = df['volt_laser'].to_numpy()
 volt_piezo = df['volt_piezo'].to_numpy()
 volt_ld = df['volt_ld'].to_numpy()
 
-# Use it in case of mode hopping
-'''mask = (timestamps >= -0.01)
-timestamps = timestamps[mask]
-volt_laser = volt_laser[mask]
-volt_piezo = volt_piezo[mask]
-volt_ld = volt_ld[mask]'''
+# Extra cropping, use it in case of mode hopping
+if cropping:
+    mask = (timestamps >= -0.01)
+    timestamps = timestamps[mask]
+    volt_laser = volt_laser[mask]
+    volt_piezo = volt_piezo[mask]
+    volt_ld = volt_ld[mask]
 
 # Cropping data to a single sweep
-# timestamps, volt_laser, volt_piezo, volt_ld = fn.crop_to_min_max(
-#     timestamps, volt_laser, volt_piezo, volt_ld)
-# If the data is acquired with modulation both on piezo and laser diode current:
+# NOTICE, VOLT PIEZO HAS TO BE THE FIRST ONE
 volt_piezo, timestamps, volt_laser, volt_ld = fn.crop_to_min_max(
     volt_piezo, timestamps, volt_laser, volt_ld)
 
@@ -65,19 +73,19 @@ ld_fitted = fn.fit_piezo_line(timestamps, volt_ld)
 
 # Plotting, just for fun
 plt.figure()
-plt.plot(timestamps, volt_laser, label='Laser intensity',
-         color='blue', markersize=5, marker='.')
-plt.plot(timestamps, piezo_fitted/10, label='Piezo voltage/10',
-         color='red', markersize=5, marker='.')
-plt.plot(timestamps, volt_ld, label='Diode modulation',
-         color='green', markersize=5, marker='.')
+plt.scatter(timestamps, volt_laser, label='Laser intensity',
+            color='blue', s=5, marker='.')
+plt.scatter(timestamps, piezo_fitted/10, label='Piezo voltage/10',
+            color='red', s=5, marker='.')
+plt.scatter(timestamps, volt_ld, label='Diode modulation',
+            color='green', s=5, marker='.')
 plt.xlabel('Timestamp [s]')
 plt.ylabel('Channel Value [V]')
 plt.title('Timestamp vs Channel Data')
 plt.grid()
 plt.legend()
 plt.tight_layout()
-plt.savefig(f'{folder_name}/figures/simple_plots_time/{title}.pdf')
+plt.savefig(f'{folder_name}/figures/simple_plots_time/{title}.png')
 plt.show()
 
 # Saving data in clean_data folder
